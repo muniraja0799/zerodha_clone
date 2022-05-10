7434d665632c4ce917578dc411d21ad779ef3ce0
Muniraja <muniraja0799@gmail.com>
2022-05-10 18:06:21 +0530

    version 1.0

---
+++ android/app/src/main/kotlin/xyz/mandroid/zerodha_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.mandroid.zerodha_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zerodha Clone',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Zerodha'),
+        ),
+        body: const Center(
+          child: Text('Hello Muniraja'),
+        ),
+      ),
+    );
+  }
+}
